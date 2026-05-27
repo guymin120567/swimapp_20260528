ef7b757a6ce4a3c9969e51d9c9502668c1bfb2d2
guymin120567 <guymin120567@gmail.com>
2026-05-28 03:31:36 +0900

    Update actions.js

--- js/state/actions.js
+++ js/state/actions.js
@@ -3,6 +3,56 @@ import {
 } from "./state.js";
 
 // =========================
+// ADD
+// =========================
+
+export function addCap(item){
+
+  getState()
+    .data
+    .caps
+    .push(item);
+
+}
+
+export function addSwim(item){
+
+  getState()
+    .data
+    .swimsuits
+    .push(item);
+
+}
+
+// =========================
+// REMOVE
+// =========================
+
+export function removeCap(id){
+
+  const state =
+    getState();
+
+  state.data.caps =
+    state.data.caps.filter(
+      v => v.id !== id
+    );
+
+}
+
+export function removeSwim(id){
+
+  const state =
+    getState();
+
+  state.data.swimsuits =
+    state.data.swimsuits.filter(
+      v => v.id !== id
+    );
+
+}
+
+// =========================
 // SELECTION
 // =========================
 
@@ -21,7 +71,7 @@ export function setSelectedSwim(id){
 }
 
 // =========================
-// ACTIVE UI
+// ACTIVE
 // =========================
 
 export function setActiveCap(id){
