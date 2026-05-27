0469fc902245d467611dcd0886be67934d857ef8
guymin120567 <guymin120567@gmail.com>
2026-05-28 04:56:20 +0900

    Update controller.js

--- js/core/controller.js
+++ js/core/controller.js
@@ -1,9 +1,11 @@
-import { setState, defaultState } from "../state/state.js";
+import { setState, defaultState, subscribe } from "../state/state.js";
 import { renderLayout } from "../ui/renderLayout.js";
 import { initDOM } from "../ui/dom.js";
 import { loadState } from "../../db/database.js";
 import { initTabs } from "../ui/tabs.js";
 import { bindGlobal } from "../ui/events.js";
+import { renderLists } from "../features/lists/renderLists.js";
+import { renderCoverflow } from "../features/coverflow/renderCoverflow.js";
 
 export function initController(){
 
@@ -15,15 +17,19 @@ export function initController(){
 
     setState(saved || defaultState);
 
-    // UI 생성
     renderLayout();
-
-    // DOM 캐시
     initDOM();
 
-    // 이벤트
     initTabs();
     bindGlobal();
+
+    // 🔥 핵심: subscribe 연결
+    subscribe(renderLists);
+    subscribe(renderCoverflow);
+
+    // 최초 렌더
+    renderLists();
+    renderCoverflow();
 
     console.log("BOOT DONE");
   }
