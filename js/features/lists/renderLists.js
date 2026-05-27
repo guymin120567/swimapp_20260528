e6918b9cf081b2107992cf37eb368d2b164e8ba8
guymin120567 <guymin120567@gmail.com>
2026-05-28 04:58:28 +0900

    Update renderLists.js

--- js/features/lists/renderLists.js
+++ js/features/lists/renderLists.js
@@ -1,40 +1,15 @@
-import {
-  renderCoverflow
-} from "../coverflow/renderCoverflow.js";
-
-import {
-  renderCapsList
-} from "./renderCapsList.js";
-
-import {
-  renderSwimsList
-} from "./renderSwimsList.js";
+import { renderCapsList } from "./renderCapsList.js";
+import { renderSwimsList } from "./renderSwimsList.js";
 
 export function renderLists(){
 
-  const target =
-    document.getElementById(
-      "listsContent"
-    );
-
+  const target = document.getElementById("listsContent");
   if(!target) return;
 
   target.innerHTML = `
-
     <div class="lists-wrap">
-
       ${renderCapsList()}
-
       ${renderSwimsList()}
-
     </div>
-
   `;
-
-  requestAnimationFrame(()=>{
-
-    renderCoverflow();
-
-  });
-
 }
