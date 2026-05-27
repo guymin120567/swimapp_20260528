c9d60ffa4f666e94b9935f3b18e062f018425b0a
guymin120567 <guymin120567@gmail.com>
2026-05-28 03:10:09 +0900

    Create renderCapsList.js

---
+++ js/features/lists/renderCapsList.js
@@ -0,0 +1,58 @@
+import {
+  getState
+} from "../state/state.js";
+
+export function renderCapsList(){
+
+  const state =
+    getState();
+
+  return `
+
+    <section class="list-block">
+
+      <div class="list-title">
+        CAPS
+      </div>
+
+      <div
+        id="capCoverflow"
+        class="coverflow"
+      >
+
+        ${
+          state.data.caps.map(item => `
+
+            <div
+              class="
+                cover-card
+                ${
+                  item.id ===
+                  state.ui.activeCapId
+                  ? "active"
+                  : ""
+                }
+              "
+              data-id="${item.id}"
+            >
+
+              <img
+                src="${item.image}"
+                alt="${item.name}"
+              />
+
+              <div class="cover-name">
+                ${item.name}
+              </div>
+
+            </div>
+
+          `).join("")
+        }
+
+      </div>
+
+    </section>
+
+  `;
+}
