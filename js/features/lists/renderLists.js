fc9aebf8c65f13c4840550dd7128d6b8cc32a343
guymin120567 <guymin120567@gmail.com>
2026-05-29 03:34:34 +0900

    Update renderLists.js

--- js/features/lists/renderLists.js
+++ js/features/lists/renderLists.js
@@ -1,15 +1,3 @@
-import { renderCapsList } from "./renderCapsList.js";
-import { renderSwimsList } from "./renderSwimsList.js";
-
 export function renderLists(){
-
-  const target = document.getElementById("listsContent");
-  if(!target) return;
-
-  target.innerHTML = `
-    <div class="lists-wrap">
-      ${renderCapsList()}
-      ${renderSwimsList()}
-    </div>
-  `;
+  return;
 }
